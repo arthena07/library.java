066d31371089157f065ccd36748e45ad0295b316
Sunendi <nendi165@gmail.com>
2025-12-07 14:11:10 +0700

    resolve conflict

--- run_program.bat
+++ run_program.bat
@@ -1,12 +1,12 @@
-<<<<<<< HEAD
+
 @echo off
 echo Running Library Management System...
 echo.
 "C:\Program Files\Java\jdk-17\bin\java.exe" Main < input.txt
-=======
+
 @echo off
 echo Running Library Management System...
 echo.
 "C:\Program Files\Java\jdk-17\bin\java.exe" Main < input.txt
->>>>>>> 40f4c8c4ecb9f43fc88a3eab68f094b13c58732b
+
 pause
--- run_simple.bat
+++ run_simple.bat
@@ -1,12 +1,12 @@
-<<<<<<< HEAD
+
 @echo off
 echo Running Library Management System with simple input...
 echo.
 "C:\Program Files\Java\jdk-17\bin\java.exe" Main < simple_input.txt
-=======
+
 @echo off
 echo Running Library Management System with simple input...
 echo.
 "C:\Program Files\Java\jdk-17\bin\java.exe" Main < simple_input.txt
->>>>>>> 40f4c8c4ecb9f43fc88a3eab68f094b13c58732b
+
 pause
